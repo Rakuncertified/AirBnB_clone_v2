25661c2714c5bde657f3fdeed52e0154fa18f05c
Confidence Ehiemere <118529046+Confytech@users.noreply.github.com>
2023-06-22 03:09:59 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
-"""This is the city class"""
+"""This is the state class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import Place
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
-class City(BaseModel, Base):
-    """This is the class for City
+class State(BaseModel, Base):
+    """This is the class for State
     Attributes:
-        state_id: The state id
         name: input name
     """
-    __tablename__ = "cities"
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
